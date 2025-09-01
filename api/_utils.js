f6a433beb7060c1ae3a3ed9e4647e33c3f603491
thedeucetrey <doug.ywdc@gmail.com>
2025-09-01 18:48:04 -0400

    Create _utils.js

---
+++ api/_utils.js
@@ -0,0 +1,15 @@
+module.exports.readJson = async function readJson(req) {
+  if (req.body && typeof req.body === 'object') return req.body;
+  return await new Promise(resolve => {
+    let data = '';
+    req.on('data', chunk => { data += chunk; });
+    req.on('end', () => {
+      try { resolve(JSON.parse(data || '{}')); } catch { resolve({}); }
+    });
+  });
+};
+module.exports.cors = function cors(res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://thedeucetrey.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+};
