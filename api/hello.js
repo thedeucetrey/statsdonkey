b49036cc3d9d2a731a926aa5828913ff0503bedc
thedeucetrey <doug.ywdc@gmail.com>
2025-09-01 20:28:12 -0400

    Delete api directory

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-// statsdonkey/api/hello.js
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true, now: Date.now() }));
-};
